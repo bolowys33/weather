0162057dfdd0a1803662a31a020da08ad0a41e3c
Olubodun <bolowys33@gmail.com>
2025-05-16 12:31:13 +0100

    feat: implement weather fetching functionality in App component and enhance InputSection with props

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,38 @@
 import { useState } from "react";
 import Navbar from "./components/Navbar";
 import InputSection from "./components/InputSection";
+import axios from "axios";
+
+const appKey = import.meta.env.VITE_APP_KEY;
+const url = import.meta.env.VITE_BASE_URL;
 
 function App() {
+  const [searchText, setSearchText] = useState("");
+  const [data, setData] = useState(null);
+
+  const getWeather = async () => {
+    try {
+      const response = await axios.get(url, {
+        params: {
+          q: searchText,
+          appid: appKey,
+        },
+      });
+      setData(response.data);
+      console.log(response.data); 
+    } catch (error) {
+      console.error("Error fetching weather:", error);
+    }
+  };
+
   return (
     <div className="bg-[skyblue] text-[#333333] h-screen">
       <Navbar />
-      <InputSection />
+      <InputSection
+        value={searchText}
+        onChange={(e) => setSearchText(e.target.value)}
+        onSubmit={getWeather}
+      />
     </div>
   );
 }
--- src/components/InputSection.jsx
+++ src/components/InputSection.jsx
@@ -1,11 +1,20 @@
-const InputSection = () => {
+const InputSection = ({ value, onChange, onSubmit }) => {
   return (
     <div className="w-full flex flex-col items-center justify-center gap-4 mt-[40px]">
       <input
         type="text"
-        className=" bg-[#F0F0F0] h-[40px] w-[350px] rounded-full outline-[#007BFF] border-2 border-[#CCCCCC] px-3 text-[16px]"
+        className="bg-[#F0F0F0] h-[40px] w-[350px] rounded-full outline-[#007BFF] border-2 border-[#CCCCCC] px-3 text-[16px]"
+        value={value}
+        placeholder="Enter city to get weather"
+        onChange={onChange}
       />
-      <button className="bg-[#004E89] px-4 py-3 text-white rounded-xl font-semibold text-[16px] cursor-pointer">
+      <button
+        onClick={onSubmit}
+        className={`bg-[#004E89] px-4 py-3 text-white rounded-xl font-semibold text-[16px] ${
+          !value ? "cursor-not-allowed" : "cursor-pointer"
+        }`}
+        disabled={!value}
+      >
         Get Weather
       </button>
     </div>
